e6b775ecbc9c98de318ac1f05f678ac6d7471c58
BlaineParr <Blaine.parr@hotmail.com>
2015-03-11 21:30:12 -0400

    Created base game

---
+++ COMP2068-Assignment3/Scripts/objects/tank.ts
@@ -0,0 +1,19 @@
+﻿module objects {
+    export class Tank extends createjs.Bitmap{
+        //Constructor/////////////////////////////////////////////////////////////////////////////
+        constructor() {
+            super(assetLoader.getResult("tank"));
+
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
+
+            this.x = 32;
+            this.y = stage.mouseY;
+        } //constructor ends
+
+        //Public Methods//////////////////////////////////////////////////////////////////////////
+        update() {
+            this.y = stage.mouseY;
+        } //method update ends
+    } //class Tank ends
+} //module objects ends 
